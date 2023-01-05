648be4eeb70ba07e805b6e9b37fe7c05097ddcb5
ahmedsalihh <ahmedsalihh@gmail.com>
2023-01-06 01:31:30 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const [css, setCss] = useState();
+  const [html, setHtml] = useState();
+
+  function createMarkup() {
+    return { __html: html };
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='editors'>
+        <textarea
+          value={css}
+          style={{ minWidth: '20vw', minHeight: '40vh' }}
+          onChange={event => setCss(event.target.value)}
+        />
+        <textarea
+          value={html}
+          style={{ minWidth: '20vw', minHeight: '40vh' }}
+          onChange={event => setHtml(event.target.value)}
+        />
+      </div>
+      <div className='test' style={{ width: '100%', minHeight: '80vh'}}>
+        <style>{css}</style>
+        <div className='demo' dangerouslySetInnerHTML={createMarkup()} />
+      </div>
     </div>
   );
 }
